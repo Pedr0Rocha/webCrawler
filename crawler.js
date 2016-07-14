602f5d9ccdfa5fd63bfbf2ed4b734f301375f94b
pedronote <pedr0_r@hotmail.com>
2016-07-14 04:17:40 -0300

    Parameters are from command line now. Added check app ranking crawl

--- crawler.js
+++ crawler.js
@@ -5,22 +5,38 @@
 */
 
 /*
-====== Dependencies ========
-*/
-var request = require('request'); // Request is used to make HTTP requests.
-var cheerio = require('cheerio'); // Cheerio is used to parse and select HTML elements on the page.
-var URL     = require('url-parse'); // URL is used to parse URLs.
-
-/*
 ====== Settings ========
 */
-var URL_TO_CRAWL = "http://www.url.com";
+var URL_TO_CRAWL;
 var KEYWORD = "test";
 var MAX_PAGES_TO_VISIT = 100;
 var CRAWL_TIL_YOU_DIE = true;
-var LINK_TYPE = 1;   // 1 - Only relative links
-					 // 2 - Only absolute links
-					 // 3 - Both
+var LINK_TYPE = 1;   // 1 - relative links, 2 - absolute links, 3 - Both
+
+if (!process.argv[2]) {
+	console.log("No URL to crawl.");
+	return;
+} else {
+	URL_TO_CRAWL = process.argv[2];
+}
+if (!process.argv[3])
+	console.log("No crawl type, default is 1 (relative links).");
+else
+	LINK_TYPE = process.argv[3];
+
+if (LINK_TYPE == 99)
+	URL_TO_CRAWL = "https://play.google.com/store/search?q=munchkin";
+
+if (!process.argv[4])
+	console.log("No Keyword.");
+else
+	KEYWORD = process.argv[4];
+/*
+====== Dependencies ========
+*/
+var request = require('request'); 
+var cheerio = require('cheerio'); 
+var URL     = require('url-parse');
 
 /*
 ====== Run ========
@@ -33,7 +49,6 @@ var baseUrl = url.protocol + "//" + url.hostname;
 
 pagesToVisit.push(URL_TO_CRAWL);
 crawl();
-
 
 /*
 ====== Crawler ========
@@ -60,20 +75,22 @@ function visitPage(url, callback) {
 
 	console.log("Crawling: " + url);
 	request(url, function(error, response, body) {
-		//console.log("Status code: " + response.statusCode);
 		if(response == null || response.statusCode !== 200) {
-			//console.log("Error! " + error);
+			console.log("Server Response " + error);
 			callback();
 			return;
 		}
 
 		var $ = cheerio.load(body);
+		if (LINK_TYPE == 99) {
+			checkAppRank($);
+			return;
+		}
+
 		//console.log("Page Title: " + $('title').text());
 		var isWordFound = searchForWord($, KEYWORD);
 		if (isWordFound) {
-			console.log('===================================');
-			console.log('Keyword ' + KEYWORD + ' found at ' + url);
-			console.log('===================================');
+			printFoundWord(KEYWORD, url);
 			if (CRAWL_TIL_YOU_DIE || numPagesVisited <= MAX_PAGES_TO_VISIT) {
 				collectLinks($);
 				callback();
@@ -83,6 +100,12 @@ function visitPage(url, callback) {
 			callback();
 		}
 	});
+}
+
+function printFoundWord(keyword, url) {
+	console.log('===================================');
+	console.log('Keyword ' + keyword + ' found at ' + url);
+	console.log('===================================');
 }
 
 function searchForWord($, keyword) {
@@ -105,6 +128,25 @@ function collectAbsoluteLinks($) {
     	//console.log($(this).attr('href'));
         pagesToVisit.push($(this).attr('href'));
     });
+}
+
+function checkAppRank($) {
+	var apps = $(".card.no-rationale.square-cover.apps.small .details .title");
+	var developers = $(".card.no-rationale.square-cover.apps.small .details .subtitle");
+	var position = 0;
+	var index = 1;
+	apps.each(function() {
+		var appName = $(this).attr('title');
+		var devName = $($(this).next().children()[0]).attr('title');
+		console.log(index + " - " + appName + " by " + devName);
+		if (devName == "PedroRocha")
+			position = index;
+		index++;
+	});
+	console.log("=================================");
+	console.log("App is in position " + position + " today.");
+	console.log("=================================");
+	return;
 }
 
 function collectLinks($){
